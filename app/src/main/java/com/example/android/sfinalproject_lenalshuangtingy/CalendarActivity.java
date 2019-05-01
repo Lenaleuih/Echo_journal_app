8a1deb63a63cbe988bd19dbf7643eda49bd8db17
Lenaleuih <lenaleuih@163.com>
2019-05-01 19:03:26 -0400

    updated the calendar view

--- app/src/main/java/com/example/android/sfinalproject_lenalshuangtingy/CalendarActivity.java
+++ app/src/main/java/com/example/android/sfinalproject_lenalshuangtingy/CalendarActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.Toast;
@@ -38,6 +40,34 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.launchSettings2:
+                Intent intent2 = new Intent(this, SettingActivity.class);
+                startActivity(intent2);
+                return true;
+
+//            case R.id.calendarView:
+//                Intent intent = new Intent(this, CalendarActivity.class);
+//                startActivity(intent);
+//                return true;
+            // to do: search function
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.secondmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 
